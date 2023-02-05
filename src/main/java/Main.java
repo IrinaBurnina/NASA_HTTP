b6da56ead1c908cbce63d3f26b31874ed00ae407
Irina Burnina <irinkaburninka@gmail.com>
2023-02-06 00:54:01 +0300

    correct Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.apache.http.ParseException;
+import com.google.gson.JsonIOException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedOutputStream;
@@ -14,10 +12,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -33,55 +27,31 @@ public class Main {
         HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=8aegeJ2xbKhMcdXkuYhGKJw9u6K3cjrqyhtIGVmV");
         CloseableHttpResponse response = httpClient.execute(request);
         String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
-        String bodyBuildToArray = "[" + body + "]";
-        List<News> newsList = jsonToList(bodyBuildToArray);
-        List<String> urls = new ArrayList<>();
-        for (News newsInfo : newsList) {
-            String url = newsInfo.getUrl();
-            urls.add(url);
-        }
-        for (int i = 0; i < urls.size(); i++) {
-            HttpGet requestByURL = new HttpGet(urls.get(i));
-            CloseableHttpResponse responseURL = httpClient.execute(requestByURL);
-            byte[] contentNews = responseURL.getEntity().getContent().readAllBytes();
-            List<String> strings = urls.stream()
-                    .flatMap(value -> Arrays.stream(value.split("/")))
-                    .collect(Collectors.toList());
-            String newFileName = strings.get(strings.size() - 1);
-            File file = new File(newFileName);
-            try {
-                file.createNewFile();
-                try (FileOutputStream fos = new FileOutputStream(newFileName);
-                        BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-                    bos.write(contentNews, 0, contentNews.length);
-                } catch (IOException ex) {
-                    System.out.println(ex.getMessage());
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-
-        }
-    }
-
-
-    public static List<News> jsonToList(String json) {
-        List<News> list = new ArrayList<>();
-        JSONParser parser = new JSONParser();
+        String fileName = null;
+        String url = null;
         try {
-            JSONArray jsonArray = (JSONArray) parser.parse(json);
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-
-            for (Object newsNASA : jsonArray) {
-                News news = gson.fromJson(String.valueOf(newsNASA), News.class);
-                list.add(news);
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (org.json.simple.parser.ParseException e) {
-            throw new RuntimeException(e);
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(body);
+            url = jsonObject.get("url").toString();
+            String[] strings = url.split("/");
+            fileName = strings[strings.length - 1];
+        } catch (JsonIOException | org.json.simple.parser.ParseException err) {
+            System.out.println(err.getMessage());
         }
-        return list;
+        HttpGet requestByURL = new HttpGet(url);
+        CloseableHttpResponse responseURL = httpClient.execute(requestByURL);
+        byte[] contentNews = responseURL.getEntity().getContent().readAllBytes();
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+            try (FileOutputStream fos = new FileOutputStream(fileName);
+                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                bos.write(contentNews, 0, contentNews.length);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

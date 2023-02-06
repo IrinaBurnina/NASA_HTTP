fc64a85f7de52a189903f8197ec73ed8cae4973b
Irina Burnina <irinkaburninka@gmail.com>
2023-02-06 13:24:18 +0300

    correct News.java use JsonProperty

--- src/main/java/News.java
+++ src/main/java/News.java
@@ -1,22 +1,25 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class News {
     private final String copyright;
     private final String date;
     private final String explanation;
     private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
-    public News(String copyright, String date, String explanation, String hdurl,
-                String media_type, String service_version,
-                String title, String url) {
+    public News(@JsonProperty("copyright") String copyright, @JsonProperty("date") String date,
+                @JsonProperty("explanation") String explanation, @JsonProperty("hdurl") String hdurl,
+                @JsonProperty("media_type") String mediaType, @JsonProperty("service_version") String serviceVersion,
+                @JsonProperty("title") String title, @JsonProperty("url") String url) {
         this.copyright = copyright;
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -32,8 +35,8 @@ public class News {
                 ", date=" + date +
                 ", explanation='" + explanation + '\'' +
                 ", hdurl='" + hdurl + '\'' +
-                ", media_type='" + media_type + '\'' +
-                ", service_version='" + service_version + '\'' +
+                ", media_type='" + mediaType + '\'' +
+                ", service_version='" + serviceVersion + '\'' +
                 ", title='" + title + '\'' +
                 ", url='" + url + '\'' +
                 '}';
